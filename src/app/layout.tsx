4f7285bbd0510d7ceb7c8c5e537efd2cfcaf000d
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-03-27 22:05:34 +0430

    Add configs for commit message.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Create Next App',
+  title: 'NextJS',
   description: 'Generated Amir Ali Azimloo',
 };
 
